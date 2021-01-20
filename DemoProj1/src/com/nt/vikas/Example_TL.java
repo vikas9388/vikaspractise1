3dcefbc2498a60e3cad9814ae838ce5f1a58dde3
GITTL <gittl@gmail.com>
2021-01-20 16:04:15 +0530

    time upgradation

--- DemoProj1/src/com/nt/vikas/Example_TL.java
+++ DemoProj1/src/com/nt/vikas/Example_TL.java
@@ -1,5 +1,6 @@
 package com.nt.vikas;
 
+import java.time.LocalTime;
 import java.util.Date;
 
 public class Example_TL {
@@ -7,6 +8,9 @@ public class Example_TL {
 	public static void main(String[] args) {
 		Date d1=new Date();
 		System.out.println("Todays date is:"+d1);
+		LocalTime lt=LocalTime.now();
+		System.out.println("Current time is:"+lt);
+		
 	}
 
 }
