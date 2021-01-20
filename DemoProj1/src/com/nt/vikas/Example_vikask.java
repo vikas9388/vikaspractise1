c820c51269172692d07caaf839fbd790957f5808
GITTL <gittl@gmail.com>
2021-01-20 15:18:00 +0530

    Initial import by DEV1

---
+++ DemoProj1/src/com/nt/vikas/Example_vikask.java
@@ -0,0 +1,16 @@
+package com.nt.vikas;
+
+public class Example_vikask {
+	int a=20,b=30;
+	public void m1() {
+		int c=a+b;
+		System.out.println("The addition of a and b is:"+c);
+	}
+	public static void main(String[] args) {
+		
+		Example_vikask vik=new Example_vikask();
+		vik.m1();
+		
+	}
+
+}
