d97a3607ad1d7090efd347ad71dbbfa374641e81
GITTL <gittl@gmail.com>
2021-01-20 15:27:37 +0530

    Date class push

---
+++ DemoProj1/src/com/nt/vikas/Example_TL.java
@@ -0,0 +1,12 @@
+package com.nt.vikas;
+
+import java.util.Date;
+
+public class Example_TL {
+
+	public static void main(String[] args) {
+		Date d1=new Date();
+		System.out.println("Todays date is:"+d1);
+	}
+
+}
