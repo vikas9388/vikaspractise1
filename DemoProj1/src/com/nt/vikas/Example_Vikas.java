6225513749629e13b9f4fd4fcd39eec7e8e48900
GITTL <gittl@gmail.com>
2021-01-20 14:59:24 +0530

    Initial Import

---
+++ DemoProj1/src/com/nt/vikas/Example_Vikas.java
@@ -0,0 +1,15 @@
+package com.nt.vikas;
+
+import java.util.Scanner;
+
+public class Example_Vikas {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Plz enter first name");
+		String name=sc.nextLine();
+		System.out.println("The entered name is:"+name);
+
+	}
+
+}
