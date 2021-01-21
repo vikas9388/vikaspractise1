f2b03604128d1c516c7b6b5734c45e2e3a3d93d1
GITDEV <gitdev@gmail.com>
2021-01-21 12:33:13 +0530

    Example_TL class modification by TL

--- DemoProj1/src/com/nt/vikas/Example_TL.java
+++ DemoProj1/src/com/nt/vikas/Example_TL.java
@@ -2,15 +2,19 @@ package com.nt.vikas;
 
 import java.time.LocalTime;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Example_TL {
 
 	public static void main(String[] args) {
-		Date d1=new Date();
+		/*Date d1=new Date();
 		System.out.println("Todays date is:"+d1);
 		//LocalTime lt=LocalTime.now();
-		System.out.println("Current time is:"+LocalTime.now());
-		
+		System.out.println("Current time is:"+LocalTime.now());*/
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Plz enter first name");
+		String name=sc.nextLine();
+		System.out.println("The entered name is:"+name);
 		
 	}
 
