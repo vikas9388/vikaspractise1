eab74b1966a914e0120f95e9dce0fbdca22fce7c
GITDEV <gitdev@gmail.com>
2021-01-21 12:25:07 +0530

    comment plus scanner class object added

--- DemoProj1/src/com/nt/vikas/Example_TL.java
+++ DemoProj1/src/com/nt/vikas/Example_TL.java
@@ -1,16 +1,16 @@
 package com.nt.vikas;
 
-import java.time.LocalTime;
-import java.util.Date;
+import java.util.Scanner;
 
 public class Example_TL {
 
 	public static void main(String[] args) {
-		Date d1=new Date();
+		/*Date d1=new Date();
 		System.out.println("Todays date is:"+d1);
 		//LocalTime lt=LocalTime.now();
 		System.out.println("Current time is:"+LocalTime.now());
-		
+		*/
+		Scanner sc=new Scanner(System.in);
 		
 	}
 
